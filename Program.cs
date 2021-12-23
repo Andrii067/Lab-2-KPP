9af8e0616767ec85d917a322aa372a96d3d36389
unknown <20fi.a.zuienko@std.npu.edu.ua>
2021-12-23 12:26:57 +0200

    Signed-off-by: unknown <20fi.a.zuienko@std.npu.edu.ua>

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace System
+{
+    class Rain {
+
+
+        public string Weather = "Дождь";
+        public string region = "Київ";
+        public int Vise = 754;
+        public int Wind = 1;
+
+        public void Print()
+        {
+            Console.WriteLine("Погода:"+ Weather);
+            Console.WriteLine("Регiон:"+ region);
+            Console.WriteLine("Тиск:"+ Vise + "мм рт. ст.");
+            Console.WriteLine("Вiтер:"+ Wind + "м/c");
+        }
+        
+    }
+    class Snowfall
+    {
+
+
+        public string Weather = "Cнiгопад";
+        public string region = "Харкiв";
+        public int Vise = 749;
+        public int Wind = 8;
+
+        public void Print()
+        {
+            Console.WriteLine("\nПогода:" + Weather);
+            Console.WriteLine("Регiон:" + region);
+            Console.WriteLine("Тиск:" + Vise + "мм рт. ст.");
+            Console.WriteLine("Вiтер:" + Wind + "м/c");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rain KYIV = new Rain();
+            KYIV.Print();
+
+            Snowfall Kharkiv = new Snowfall();
+            Kharkiv.Print();
+        }
+    }
+}
